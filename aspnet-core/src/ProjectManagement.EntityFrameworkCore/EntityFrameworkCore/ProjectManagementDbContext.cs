76925db5eff712cfbee025c3245fa86f6ac708d9
Anthony Nguyen <thang.nguyen1@fractal.vn>
2020-07-18 16:39:03 +0700

    project intial

---
+++ aspnet-core/src/ProjectManagement.EntityFrameworkCore/EntityFrameworkCore/ProjectManagementDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Abp.Zero.EntityFrameworkCore;
+using ProjectManagement.Authorization.Roles;
+using ProjectManagement.Authorization.Users;
+using ProjectManagement.MultiTenancy;
+
+namespace ProjectManagement.EntityFrameworkCore
+{
+    public class ProjectManagementDbContext : AbpZeroDbContext<Tenant, Role, User, ProjectManagementDbContext>
+    {
+        /* Define a DbSet for each entity of the application */
+        
+        public ProjectManagementDbContext(DbContextOptions<ProjectManagementDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ aspnet-core/src/ProjectManagement.Web.Host/Startup/ProjectManagementWebHostModule.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Abp.Modules;
+using Abp.Reflection.Extensions;
+using ProjectManagement.Configuration;
+
+namespace ProjectManagement.Web.Host.Startup
+{
+    [DependsOn(
+       typeof(ProjectManagementWebCoreModule))]
+    public class ProjectManagementWebHostModule: AbpModule
+    {
+        private readonly IWebHostEnvironment _env;
+        private readonly IConfigurationRoot _appConfiguration;
+
+        public ProjectManagementWebHostModule(IWebHostEnvironment env)
+        {
+            _env = env;
+            _appConfiguration = env.GetAppConfiguration();
+        }
+
+        public override void Initialize()
+        {
+            IocManager.RegisterAssemblyByConvention(typeof(ProjectManagementWebHostModule).GetAssembly());
+        }
+    }
+}
