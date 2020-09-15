adf390dc616710a81317c12fa5ee2e9461f266bf
Anthony Nguyen <thang.nguyen1@fractal.vn>
2020-09-15 23:52:12 +0700

    update teacher page

--- applications/student-web/src/environments/environment.prod.ts
+++ applications/student-web/src/environments/environment.prod.ts
@@ -1,3 +1,24 @@
 export const environment = {
-  production: true
+  production: true,
+  application: {
+    name: 'ProjectManagement',
+    logoUrl: '',
+  },
+  oAuthConfig: {
+    issuer: 'https://localhost:44338',
+    clientId: 'ProjectManagement_App',
+    dummyClientSecret: '1q2w3e*',
+    scope: 'ProjectManagement',
+    showDebugInformation: true,
+    oidc: false,
+    requireHttps: true,
+  },
+  apis: {
+    default: {
+      url: 'https://localhost:44338',
+    },
+  },
+  localization: {
+    defaultResourceName: 'ProjectManagement',
+  },
 };
