476a99c703fd25d585a2f31535243ca17ac0c39f
Anthony Nguyen <thang.nguyen1@fractal.vn>
2020-09-16 00:50:25 +0700

    add teacher user

---
+++ servicces/doan.Shared/Eto/StudentEto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace doan.Shared.Eto
+{
+    [Serializable]
+    public class StudentEto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string StudentCode { get; set; }
+        public string Email { get; set; }
+        public Guid? CurrentTenantId { get; set; }
+
+        public StudentEto()
+        {
+
+        }
+
+        public StudentEto(Guid id, string name, string studentCode, string email, Guid? currentTenantId)
+        {
+            Id = id;
+            Name = name;
+            StudentCode = studentCode;
+            Email = email;
+            CurrentTenantId = currentTenantId;
+        }
+    }
+}
+}
---
+++ servicces/src/doan.ProjectManagement.Domain/IdentityServer/StudentCreateDistributedEventHandler.cs
@@ -0,0 +1,62 @@
+﻿using doan.Shared.Eto;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.EventBus.Distributed;
+using Volo.Abp.Guids;
+using Volo.Abp.Identity;
+
+namespace doan.ProjectManagement.IdentityServer
+{
+    public class StudentCreateDistributedEventHandler : IDistributedEventHandler<StudentEto>, ITransientDependency
+    {
+
+        private readonly IdentityUserManager _userManager;
+        private readonly IdentityRoleManager _roleManager;
+        private readonly IGuidGenerator _guidGenerator;
+        private readonly IDistributedEventBus _distributedEventBus;
+
+        public StudentCreateDistributedEventHandler(IdentityUserManager userManager, IdentityRoleManager roleManager,
+            IGuidGenerator guidGenerator, IDistributedEventBus distributedEventBus)
+        {
+            _distributedEventBus = distributedEventBus;
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _guidGenerator = guidGenerator;
+        }
+
+        public async Task HandleEventAsync(StudentEto eventData)
+        {
+            if (await _userManager.FindByNameAsync(eventData.StudentCode) != null)
+            {
+                return;
+            }
+
+            var user = new Volo.Abp.Identity.IdentityUser(
+                _guidGenerator.Create(),
+                eventData.StudentCode,
+                eventData.Email,
+                eventData.CurrentTenantId)
+            {
+                Name = eventData.Name,
+            };
+
+            var role = await _roleManager.FindByIdAsync("student");
+
+            if (role != null)
+                user.AddRole(role.Id);
+
+            (await _userManager.CreateAsync(user, eventData.StudentCode)).CheckErrors();
+
+            if (role == null)
+                await _userManager.AddDefaultRolesAsync(user);
+            await _distributedEventBus.PublishAsync(new NewStudentUserEto
+            {
+                Email = eventData.Email,
+                Name = eventData.Name,
+                Password = eventData.StudentCode,
+                UserName = eventData.StudentCode
+            });
+        }
+    }
+}
